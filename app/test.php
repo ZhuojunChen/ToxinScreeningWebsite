d5a4166ad877dcf9f7313a375b996541bb15fe1a
Yusef Abouremeleh <yusef.abouremeleh@gmail.com>
2015-05-15 08:56:41 -0700

    Added missing bootstrap source

--- app/test.php
+++ app/test.php
@@ -24,35 +24,9 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   Name: <input type="text" name="name">
-   <br><br>
-   E-mail: <input type="text" name="email">
-   <br><br>
-   Website: <input type="text" name="website">
-   <br><br>
-   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-   <br><br>
-   Gender:
-   <input type="radio" name="gender" value="female">Female
-   <input type="radio" name="gender" value="male">Male
-   <br><br>
-   <input type="submit" name="submit" value="Submit"> 
-</form>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
 
 </body>
 </html>
