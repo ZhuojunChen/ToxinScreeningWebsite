f14a9fb95c2ecf768c6bdbff83ebc22f76386817
Aaron Graham <aarong4743@optonline.net>
2017-01-13 11:53:51 -0800

    Updating Windows Branch

--- app/connection.php
+++ app/connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$con = mysqli_connect("localhost","root","root","toxin_screening", "8889", "/Applications/MAMP/tmp/mysql/mysql.sock");
+$con = mysqli_connect("localhost","root","","toxin_screening");
 
 // Check connection
 if (mysqli_connect_errno())
